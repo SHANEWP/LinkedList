9bea8cc91d9cfa89a995a73a2b21b23bfb4deefa
Shane Panagakos <SP0101ME@COMCAST.NET>
2022-01-19 11:09:08 -0500

    Add removal methods

--- js/LinkedList.js
+++ js/LinkedList.js
@@ -1,3 +1,6 @@
+/**
+ * A node that holds a piece of data and a link to another node
+ */
 class Node {
   constructor(data = null, link = null) {
     this.data = data;
@@ -5,18 +8,31 @@ class Node {
   }
 }
 
+/**
+ * A collection of data stored in nodes that are linked to the next node in the list
+ */
 class LinkedList {
   constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
+    this.head = null; //First node
+    this.tail = null; //Last node
+    this.length = 0; //Node count
   }
+  /**
+   * Adds given data to the beginning of the list
+   * @param {*} data
+   * @returns The head of the list
+   */
   insertFirst(data) {
     this.head = new Node(data, this.head);
     this.tail = this.length == 0 ? this.head : this.tail;
     this.length++;
     return this.head;
   }
+  /**
+   * Adds given data to the end of the list
+   * @param {*} data
+   * @returns The head of the list
+   */
   insertLast(data) {
     if (this.head) {
       this.tail.link = new Node(data);
@@ -27,21 +43,97 @@ class LinkedList {
       return this.insertFirst(data);
     }
   }
+  /**
+   * Adds given data to the list at the given index
+   * @param {*} data
+   * @param {int} index
+   * @returns The head of the list
+   */
   insertAt(data, index) {
-    if (Number.isInteger(index)) {
+    if (Number.isInteger(index) && index >= 0) {
       if (index < this.length) {
-        let current = this.head;
-        for (let i = 0; i < index - 1; i++) {
-          current = current.link;
+        if (index > 0) {
+          let current = this.head;
+          for (let i = 0; i < index - 1; i++) {
+            current = current.link;
+          }
+          let temp = current;
+          current.link = new Node(data, temp.link);
+          this.length++;
+        } else {
+          this.insertFirst(data);
         }
-        let temp = current;
-        current.link = new Node(data, temp.link);
-        this.length++;
+        return this.head;
       } else {
         throw new Error("index out of bounds");
       }
     } else {
-      throw new Error("index must be an integer");
+      throw new Error("index must be a positive integer");
+    }
+  }
+  /**
+   * Removes the first node in the list
+   * @returns The removed node
+   */
+  removeFirst() {
+    if (this.head) {
+      let removedNode = this.head;
+      this.head = this.head.link;
+      this.length--;
+      return removedNode;
+    } else {
+      throw new Error("node does not exist");
+    }
+  }
+  /**
+   * Removes the last node in the list
+   * @returns The removed node
+   */
+  removeLast() {
+    if (this.head) {
+      let removedNode = this.tail;
+      if (this.head === this.tail) {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+      } else {
+        let current = this.head;
+        while (current.link != this.tail) {
+          current = current.link;
+        }
+        current.link = null;
+        this.tail = current;
+        this.length--;
+      }
+      return removedNode;
+    } else {
+      throw new Error("node does not exist");
+    }
+  }
+  /**
+   * Removes the first instance of the given data from the list
+   * @param {*} data
+   * @returns The removed node
+   */
+  remove(data) {
+    if (this.head) {
+      if (this.head.data === data) {
+        return this.removeFirst();
+      } else {
+        let current = this.head;
+        let removedNode = null;
+        while (current.link) {
+          if (current.link.data === data) {
+            removedNode = current.link;
+            current.link = current.link.link;
+            this.length--;
+          }
+          current = current.link;
+        }
+        return removedNode;
+      }
+    } else {
+      throw new Error("node does not exist");
     }
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 import { LinkedList } from "./LinkedList.js";
 
 const list = new LinkedList();
+// tests for exception
+// list.removeLast();
+// list.removeLast();
 
 for (let i = 1; i < 5; i++) {
   list.insertLast(`Data ${i}`);
 }
 
-console.log(list);
+list.remove("hello");
 list.insertAt("2.5", 3);
 list.insertFirst("0");
+list.insertAt("-1", 0);
+
+list.remove("-1");
+list.remove("2.5");
+list.remove(123);
+list.removeFirst();
+list.removeLast();
+
+// tests for exceptions
 // list.insertAt("something", "somewhere");
 // list.insertAt("something else", 10);
+
+console.log(list);
